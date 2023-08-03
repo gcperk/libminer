7b5d1348536037feb74a4ed7b83e66902f0e2392
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2023-08-03 13:57:22 -0700

    add more tests

--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -4,7 +4,9 @@ test_that("lib_summary returns expected results", {
   expect_equal(ncol(result), 2)
   expect_equal(names(result), c("library", "n_packages"))
   expect_type(result$library, "character")
-  expect_type(result$n_packages, "double")
+  expect_type(result$n_packages, "integer")
+  expect_gt(sum(result$n_packages), 0)
+  expect_equal(sort(result$library), sort(.libPaths()))
 })
 
 test_that("lib_summary fails appropriately", {
