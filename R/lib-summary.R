304fa5b8605cd7cb0adfec2d294b7ee4501e3221
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2023-08-03 14:13:11 -0700

    add a file size option on output

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -2,6 +2,9 @@
 #'
 #' Provides a list of number of packages per library
 #' location on your machine
+#'
+#' @param sizes TRUE / FALSE to assess the size of
+#'
 #' @return A dataframe containing count of packages with libraries
 #' @export
 #'
@@ -9,10 +12,20 @@
 #' \dontrun{
 #' lib_summary()
 #' }
-lib_summary <- function(){
+lib_summary <- function(sizes = FALSE){
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("library", "n_packages")
+
+  if(sizes) {
+    pkg_df$lib_size <- vapply(
+      pkg_df$library,
+      function(x){
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      },
+      FUN.VALUE = double(1))
+  }
   pkg_df
 }
+
